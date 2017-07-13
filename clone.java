f68ff5f423e4301c43a02bd4b356528aad4c05a6
ironluffy <skydmk21@gmail.com>
2017-07-14 00:13:57 +0900

    Create clone.java

---
+++ clone.java
@@ -0,0 +1,17 @@
+package clone_package;
+
+/**
+ * Created by ironluffy on 2017-07-14.
+ */
+ 
+// Clone this file to your github, then download it on your machineand
+// and run this code on your own java environment.
+// ex) git clone github.com/ironluffy/github_practice/new/master
+// or you can yous github desktop
+// there are many ways to do this
+
+public class clone_class {
+    public static void main(String[] args) throws Exception {
+        System.out.print("Hello Github !!!");
+    }
+}
